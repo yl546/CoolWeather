d11971261e2619ef8f1790740413c006ca26f9e6
yl546 <1017283865@qq.com>
2020-04-04 04:56:47 +0000

    add fragment

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,41 @@
+package com.coolweather.android.db;
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport{
+    private int id;
+    private  String cityName;
+    private int cityCode;
+    private int provnceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvnceId() {
+        return provnceId;
+    }
+
+    public void setProvnceId(int provnceId) {
+        this.provnceId = provnceId;
+    }
+}
